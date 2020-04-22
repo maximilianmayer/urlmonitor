8185b658702d67ab8bb7706d2c8b27af9aa66b85
Maximilian Mayer <maximilian.mayer@codecentric.de>
2020-04-22 10:36:47 +0200

    added modularized version

--- sitemonitor.rb
+++ sitemonitor.rb
@@ -8,51 +8,89 @@ require 'optparse/uri'
 
 class Sitemonitor
 
+  attr_accessor :url
+  attr_reader :url_hash
+  attr_reader :checksum
+  attr_reader :last_check
+  attr_reader :last_check_state
+  attr_reader :last_change
+  attr_reader :status
+  attr_reader :workdir
 
-  debug = false
-  site = {'url' => ARGV[0], 'url_hash' => Digest::MD5.hexdigest(ARGV[0]), 'content_checksum' => '' , 'status' => '', 'last_check' => 0, 'last_change' => 0 }
-  site_uri = URI(site['url'])
-  workdir = 'sitemon'
-  file_path = "#{workdir}/#{site['url_hash']}"
-  res = Net::HTTP.get_response(site_uri)
-  site['last_check'] =  Time.now().to_i
-
-  def prepare(workdir)
-    Dir.mkdir workdir unless File.directory?(workdir)
+  def initialize(url)
+    @url = url
+    @workdir = 'sitemon'
+    @url_hash = Digest::MD5.hexdigest(@url)
   end
+
+  def prepare(dir)
+    Dir.mkdir dir unless File.directory?(dir)
+  end
+
+  def list_pages
+    file = File.new("#{workdir}/index.json",'r' )
+    JSON.parse(file.read)
+  end
+
+  def add_to_list(data={})
+    unless data.empty?
+      store(data,"#{workdir}/index.json")
+      return {message: 'success'}
+    end
+  end
+
+  def check_page
+    res = Net::HTTP.get_response(URI(@url))
+    @last_check =  Time.now().to_i
 
   # create a checksum only if request was successful, otherwise make it an empty string
-  if res.is_a?(Net::HTTPSuccess)
-    site['content_checksum'] = Digest::MD5.hexdigest res.body
-  else
-    site['content_checksum'] = ''
-  end
-  site['status'] = res.code
-
-
-  puts site if debug
-
-  if File.exist? (file_path)
-    file = File.new(file_path, 'r' )
-    stor_data = JSON.parse(file.read)
-    file.close
-    puts "stored data:  #{stor_data}" if debug
-    puts "checking for changes on #{site_uri}"
-    print "last checked at #{Time.at(stor_data['last_check'])}"
-    unless stor_data['content_checksum'].eql? site['content_checksum']
-      print  "\t => content changed!\n"
-      site['last_change'] = site['last_check']
+    if res.is_a?(Net::HTTPSuccess)
+      @checksum = Digest::MD5.hexdigest res.body
     else
-      print "\t => no changes!\n"
-      site['last_change'] = stor_data['last_change']
+      @checksum = ''
     end
-  else
-    puts "no previous sitecheck found. creating a new one at #{file_path}"
-    site['last_change'] = site['last_check']
+    @status = res.code
+
+  # create workdir if not already
+    self.prepare @workdir
+    data ={url: @url, url_hash: @url_hash, content_checksum: @checksum, last_check: @last_check, last_change: @last_change, status: @status, last_check_state: @last_check_state}
+    file_path = "#{@workdir}/#{@url_hash}"
+    if File.exist?(file_path) # check for existing data
+      file = File.new(file_path, 'r' )
+      stored_data = JSON.parse(file.read)
+      file.close
+      puts "stored data found:  #{stored_data}" if @debug
+      unless stored_data['content_checksum'].eql? @checksum
+        @last_check_state = 'changed'
+        @last_change = @last_check
+        store(data,file_path)
+      else
+        @last_check_state = 'no changes'
+        @last_change = stored_data['last_change']
+      end
+    else
+      puts "no previous sitecheck found. creating a new one at #{file_path}"
+      @last_check_state = 'new'
+      @last_change = @last_check
+      add_to_list({url: @url, url_hash: @url_hash, date: Time.now.to_i})
+      store(data,file_path)
+    end
   end
-  file = File.new(file_path, 'w' )
-  file.write site.to_json
-  file.close
 
-
+  def store(data,file_path)
+    file = File.new(file_path, 'w' )
+    file.write data.to_json
+    file.close
+  end
 end
+
+website = ARGV[0]
+
+a = Sitemonitor.new website
+a.check_page
+
+## output here
+puts "Website to check: #{a.url}"
+puts "last checked: #{Time.at(a.last_check).to_s}"
+puts "http status: #{a.status}"
+puts "check status: #{a.last_check_state}"
